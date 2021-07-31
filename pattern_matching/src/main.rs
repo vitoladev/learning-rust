552edd4fbb40aae3dffc718d4552e118d3346705
Victor Oliveira <asaptorvi@protonmail.com>
2021-07-31 19:32:16 -0300

    pattern matching exercise

---
+++ pattern_matching/src/main.rs
@@ -0,0 +1,58 @@
+enum CitySize {
+    Town,       // approximate residents: 1_000
+    City,       // approximate residents: 10_000
+    Metropolis, // approximate residents: 1_000_000
+}
+
+struct City {
+    description: String,
+    residents: u64,
+    is_coastal: bool,
+}
+
+impl City {
+    fn new(city_size: CitySize, is_coastal: bool) -> City {
+        let (description, residents) = match city_size {
+            CitySize::Town => {
+                let residents = 1_000;
+
+                (
+                    format!("a *town* of approximately {} residents", residents),
+                    residents,
+                )
+            }
+            CitySize::City => {
+                let residents = 10_000;
+
+                (
+                    format!("a *city* of approximately {} residents", residents),
+                    residents,
+                )
+            }
+            CitySize::Metropolis => {
+                let residents = 1_000_000;
+
+                (
+                    format!("a *metropolis* of approximately {} residents", residents),
+                    residents,
+                )
+            }
+        };
+
+        City {
+            description,
+            residents,
+            is_coastal,
+        }
+    }
+}
+
+fn main() {
+    let rustville = City::new(CitySize::Metropolis, true);
+
+    println!("This city is {}", rustville.description);
+
+    if rustville.residents > 100_000 {
+        println!("Wow!");
+    }
+}
